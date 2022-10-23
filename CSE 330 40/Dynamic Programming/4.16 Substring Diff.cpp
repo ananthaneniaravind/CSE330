90e4cff60279c3c055fd94fdf761350d8f076ef9
ananthaneniaravind <112681003+ananthaneniaravind@users.noreply.github.com>
2022-10-23 14:02:50 +0530

    Update 4.16 Substring Diff.cpp

--- CSE 330 40/Dynamic Programming/4.16 Substring Diff.cpp
+++ CSE 330 40/Dynamic Programming/4.16 Substring Diff.cpp
@@ -1,80 +1,36 @@
-#include <iostream>
-#include <cmath>
-#include <string>
-#include <cstring>
-using namespace std;
-  
-int main() {
-    int T;
-    cin>>T;
-      
-    while (T--) {
-        long ans = 0, k;
-        string p, q;
-         
-        cin>>k>>p>>q;
-          
-        int n = (int) p.size();
-        long *currk = new long [2*n+1]();
-        auto st = new long [2*n+1][2]();
-        long lcs[n+1][n+1];
-        memset(lcs, 0, sizeof(lcs[0][0]) * (n+1) * (n+1));
-          
-        for (int i=0; i<=n; i++) {
-            for (int j=0; j<=n; j++) {
-                currk[i-j+n] = 0;
-                if (i >= j) {
-                    st[i-j+n][0] = i-j+1;
-                    st[i-j+n][1] = 1;
-                } else {
-                     st[i-j+n][0] = 1;
-                     st[i-j+n][1] = j-i+1;
-                }
-            }
-        }
-          
-        for(int i=0; i<=n; i++) {
-            for(int j=0; j<=n; j++) {
-                 
-                //Longest Common Substring DP
-                if (i == 0 || j == 0) {
-                    lcs[i][j] = 0;
-                    currk[i-j+n] = 0;
-                } else if (p[i-1] == q[j-1]) {
-                    lcs[i][j] = lcs[i-1][j-1] + 1;
-                } else if (p[i-1] != q[j-1]) {
-                    lcs[i][j] = lcs[i-1][j-1] + 1;
-                    currk[i-j+n]++;
-                }
-                  
-                //Sliding Window technique
-                if (currk[i-j+n] > k) {
-                    long l, m, ctr = 0, flag = 0;
-                      
-                    for(l=st[i-j+n][0], m=st[i-j+n][1]; l<i && m<j; l++, m++) {
-                        ctr++;
-                        if (p[l-1] != q[m-1]) {
-                            flag = 1;
-                            break;
-                        }
-                    }
-                      
-                    if (flag) {
-                        lcs[i][j] = lcs[i-1][j-1] - ctr + 1;
-                        st[i-j+n][0] = l+1;
-                        st[i-j+n][1] = m+1;
-                        currk[i-j+n]--;
-                    }
-                }
-                  
-                if (lcs[i][j] > ans) {
-                    ans = lcs[i][j];
-                }
-            }
-        }
-          
-        printf("%ld\n",ans);
-    }
-     
-    return 0;
-}
+#!/bin/python3
+
+def l_func(p,q,max_s):
+    n = len(q)
+    res_ar = [0]
+    count = 0
+    ans = 0
+    for i in range(n):
+        if(p[i]!=q[i]):
+            res_ar.append(i+1)
+            count += 1
+            if(count>max_s):
+                l = res_ar[-1]-res_ar[-2-max_s]-1
+                if(l>ans):ans = l
+    if(count<=max_s):
+        return n
+    return ans
+
+def check_func(p,q,s):
+    n = len(q)
+    ans = 0
+    for i in range(n):
+        if(n-i<=ans):break
+        l = l_func(p,q[i:],s)
+        if(l>ans):
+            ans = l
+    for i in range(n):
+        if(n-i<=ans):break
+        l = l_func(q,p[i:],s)
+        if(l>ans):
+            ans = l
+    return ans
+for case_t in range(int(input())):
+    str_s,p,q = input().strip().split()
+    s = int(str_s)
+    print(check_func(p,q,s))
