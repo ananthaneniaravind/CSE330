0f9b47f60ff20c22a45d13354f24f48c9c11af47
ananthaneniaravind <112681003+ananthaneniaravind@users.noreply.github.com>
2022-10-23 14:09:22 +0530

    Update 4.20 Two Robots.cpp

--- CSE 330 40/Dynamic Programming/4.20 Two Robots.cpp
+++ CSE 330 40/Dynamic Programming/4.20 Two Robots.cpp
@@ -1,92 +1,26 @@
-#define _CRT_SECURE_NO_WARNINGS
+#!/bin/python3
 
-#include <cstdio>
-#include <iostream>
-#include <vector>
-#include <algorithm>
-#include <cmath>
-#include <queue>
+def twoRobots(m, queries):
+    prev_bot = queries[0][0]
+    mintotal = 0
+    
+    containers = [0]*(m+1)
 
-using namespace std;
-
-const int Infinity = 1000000007;
-const int NotInit = -1;
-
-struct Query {
-	int from, to, dist;
-
-	Query(int from = 0, int to = 0, int dist = 0)
-		: from(from),
-		  to(to),
-		  dist(dist) {}
-
-	void scan() {
-		scanf("%d%d", &from, &to);
-		dist = abs(from - to);
-	}
-};
-
-vector<Query> queries;
-vector<vector<int>> dp;
-
-int minDistance(int x, int y) {
-	if (dp[x][y] != NotInit)
-		return dp[x][y];
-	int result = Infinity;
-	auto queryId = max(x, y);
-	auto& query = queries[queryId];
-	if (min(x, y) + 1 < queryId) {
-		result = x == queryId 
-			? minDistance(x - 1, y) 
-			: minDistance(x, y - 1);
-		result += query.dist;
-		result += abs(queries[queryId - 1].to - query.from);
-	}
-	else {
-		for (int i = 0; i < queryId-1; ++i) {
-			int prevDistance = x == queryId 
-				? minDistance(i, y) 
-				: minDistance(x, i);
-			int newDistance = i == 0 ? 0 : abs(query.from - queries[i].to);
-			result = min(result, prevDistance + newDistance + query.dist);
-		}
-	}
-	dp[x][y] = result;
-	return result;
-}
-
-void solve() {
-	int n, m;
-	scanf("%d%d", &m, &n);
-	queries.resize(n+1);
-	for (int i = 1; i <= n; ++i) {
-		queries[i].scan();
-	}
-
-	dp.resize(n+1);
-	for (int i = 0; i <= n; ++i) {
-		dp[i].assign(n+1, NotInit);
-	}
-	dp[0][1] = queries[1].dist;
-	dp[1][0] = queries[1].dist;
-
-	int ans = Infinity;
-	for (int i = 0; i <= n; ++i) {
-		ans = min(ans, minDistance(i, n));
-		ans = min(ans, minDistance(n, i));
-	}
-	printf("%d\n", ans);
-}
-
-int main(void) {
-	//freopen("input.txt", "rt", stdin);
-	//freopen("output.txt", "wt", stdout);
-
-	int t;
-	scanf("%d", &t);
-	while(t --> 0) {
-		solve();
-	}
-
-	return 0;
-}
+    for a,b in queries:
+        distance = abs(a-b)
+        traveled = abs(prev_bot-a)
+        minMoves = mintotal
+        for k,v in enumerate(containers):
+            minMoves = min( minMoves, abs(k-a)+v)
+            containers[k] += traveled+distance
+        containers[prev_bot] = minMoves+distance
+        mintotal += traveled+distance
+        prev_bot = b
+    return min(containers)
+    
+for t in range(int(input().strip())):
+    m,n=(map(int,input().rstrip().split()))
+    queries = []
+    for _ in range(n):
+        queries.append(tuple(map(int, input().rstrip().split())))
+    print(twoRobots(m, tuple(queries)))
