e8ba5c197773d726d6b9dde83d6027614391e5ef
ananthaneniaravind <112681003+ananthaneniaravind@users.noreply.github.com>
2022-10-23 13:31:42 +0530

    Update 4.03 Sherlock and Cost.cpp

--- CSE 330 40/Dynamic Programming/4.03 Sherlock and Cost.cpp
+++ CSE 330 40/Dynamic Programming/4.03 Sherlock and Cost.cpp
@@ -1,102 +1,50 @@
-#ifdef ssu1
-#define _GLIBCXX_DEBUG
-#endif
-#undef NDEBUG
-
-#include <algorithm>
-#include <functional>
-#include <numeric>
-#include <iostream>
-#include <cstdio>
-#include <cmath>
-#include <cstdlib>
-#include <ctime>
-#include <cstring>
-#include <cassert>
-#include <vector>
-#include <list>
-#include <map>
-#include <set>
-#include <deque>
-#include <queue>
-#include <bitset>
-#include <sstream>
-
-using namespace std;
-
-#define fore(i, l, r) for(int i = (l); i < (r); ++i)
-#define forn(i, n) fore(i, 0, n)
-#define fori(i, l, r) fore(i, l, (r) + 1)
-#define sz(v) int((v).size())
-#define all(v) (v).begin(), (v).end()
-#define pb push_back
-#define mp make_pair
-#define X first
-#define Y second
-
-#if ( _WIN32 || __WIN32__ )
-    #define LLD "%I64d"
-#else
-    #define LLD "%lld"
-#endif
-
-typedef long long li;
-typedef long double ld;
-typedef pair<int, int> pt;
-
-template<typename T> T abs(T a) { return a < 0 ? -a : a; }
-template<typename T> T sqr(T a) { return a*a; }
-
-const int INF = (int)1e9;
-const ld EPS = 1e-9;
-const ld PI = 3.1415926535897932384626433832795;
-
-int readInt(int l, int r){
-    int x;
-    if(scanf("%d", &x) != 1){
-        fprintf(stderr, "Expected int in range [%d, %d], but haven't found!", l, r);
-        throw;
-    }
-    if(!(l <= x && x <= r)){
-        fprintf(stderr, "Expected int in range [%d, %d], but found %d!", l, r, x);
-        throw;
-    }
-    return x;
-}
-
-int up[2], nup[2];
-
-void solve(int tid){
-    int n = readInt(1, 100000);
-    vector<int> b(n);
-    forn(i, n){
-        b[i] = readInt(1, 100) - 1;
-    }
-
-    forn(i, 2)
-        up[i] = 0;
-
-    fore(i, 1, n){
-        memset(nup, 0, sizeof nup);
-        forn(j, 2){
-            forn(k, 2){
-                nup[j] = max(nup[j], up[k] + abs((k ? b[i - 1] : 0) - (j ? b[i] : 0)));
-            }
-        }
-        memcpy(up, nup, sizeof nup);
-    }
-    cout << max(up[0], up[1]) << endl;
-}
-
-int main(){
-#ifdef ssu1
-    assert(freopen("input.txt", "rt", stdin));
-    //assert(freopen("output.txt", "wt", stdout));
-#endif
-
-    int T = readInt(1, 20);
-    forn(Ti, T){
-        solve(Ti);
-    }
-    return 0;
-}
+import math
+import os
+import random
+import re
+import sys
+ 
+#
+# Complete the 'cost' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY B as parameter.
+#
+ 
+def cost(B):
+    # Write your code here
+    N = len(B)
+    assert 1 <= N <= pow(10,5)
+    assert all(1 <= b <= 100 for b in B)
+ 
+    llen, rlen = 0, 0
+    right_pos = B[0]
+ 
+    for i in range(1,N):
+ 
+        new_llen = rlen + (right_pos - 1)
+        new_right_pos = B[i]
+        new_rlen = max(
+            llen + (new_right_pos - 1),
+            rlen + abs(new_right_pos - right_pos))
+ 
+        llen, rlen = new_llen, new_rlen
+        right_pos = new_right_pos
+ 
+    return max(llen, rlen)
+ 
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+ 
+    t = int(input().strip())
+ 
+    for t_itr in range(t):
+        n = int(input().strip())
+ 
+        B = list(map(int, input().rstrip().split()))
+ 
+        result = cost(B)
+ 
+        fptr.write(str(result) + '\n')
+ 
+    fptr.close()
